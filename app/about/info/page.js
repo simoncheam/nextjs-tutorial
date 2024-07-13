e6b402605c4f031afacc3683697ac4835aab64b4
Simon <81940817+simoncheam@users.noreply.github.com>
2024-07-13 14:34:13 -0400

    first commit

---
+++ app/about/info/page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const AboutInfo = () => {
+  return (
+    <div>
+      <h1 className='text-7xl'>AboutInfoPage</h1>
+    </div>
+  );
+};
+export default AboutInfo;
