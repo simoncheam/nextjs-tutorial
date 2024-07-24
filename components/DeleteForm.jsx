bf8ca42d760176138e112c250e0b0643b829a3c4
Simon <81940817+simoncheam@users.noreply.github.com>
2024-07-23 21:09:26 -0400

    chore: save progress - Added DeleteForm and TaskFormCustom components with async actions and form status handling.

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,10 +1,22 @@
+'use client';
 import { deleteTask } from '@/utils/actions';
+import { useFormStatus } from 'react-dom';
+
+//create submit button
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button type='submit' className='btn btn-xs btn-error' disabled={pending}>
+      {pending ? 'please wait' : 'delete'}
+    </button>
+  );
+};
 
 const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type='hidden' name='id' value={id} />
-      <button className='btn btn-error btn-xs'>delete</button>
+      <SubmitButton />
     </form>
   );
 };
