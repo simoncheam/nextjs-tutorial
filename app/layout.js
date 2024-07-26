1b478421c9a7db40c28bca3d7566bcfa171bf64c
Simon <81940817+simoncheam@users.noreply.github.com>
2024-07-26 10:50:53 -0400

    fix: layout.js providers import

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
-import Providers from './Providers';
+import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
